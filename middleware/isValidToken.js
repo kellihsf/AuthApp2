196ceb6f1c812709646bbc34299b587b00ddc48b
Kelli <kellistone217@gmail.com>
2022-03-06 16:50:29 -0700

    Created middleware file to hold token

---
+++ middleware/isValidToken.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const isValidToken = (req, res, next) => {
+    const token = req.cookies["token"]
+  
+    if(token) {
+      jwt.verify(
+        token,
+        process.env.SECRET_KEY,
+        function(err, decoded){
+          if(decoded) {
+            console.log("this is the payload with the token", decoded)
+            next()
+          } else {
+            res.redirect('/error')
+          }
+        }
+      )
+    }
+    else {
+      res.redirect('/error')
+    }
+  }
+
+  module.exports = isValidToken;
