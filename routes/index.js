97386e493994d21379cb74bbc18ef39966240db5
Kelli <kellistone217@gmail.com>
2022-03-08 21:25:26 -0700

    Added axios to access outside API for practice rendering to screen

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,33 @@ var express = require('express');
 var router = express.Router();
 require('dotenv').config()
 const isValidToken = require('../middleware/isValidToken')
-const {User} = require('../models');
+const axios = require('axios')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// Get with Axios then render to screen
+router.get('/', async function (req, res, next) {
+  // Get data from remote API 
+  // Save as variable
+  let config = {
+    method: "get",
+    url: "https://swapi.dev/api/people/2",
+    headers: {},
+  }
+
+  const person = await axios(config)
+  .then(function (response) {
+    return response.data;
+  })
+  .catch(function(error) {
+    console.log(error)
+  })
+  console.log(person)
+  res.render("index", {title: "Star Wars Characters", person})
+})
 
 //Login page
 router.get('/login', function(req, res, next) {
